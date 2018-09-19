b454d893220c9877e47d094d51b02c46572272f2
guillaumedauplat <guillaume.dauplat@gmail.com>
2018-09-19 19:25:37 +0200

    route nesté

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/about', to: 'pages#about', as: :about
-  get 'pages/experience'
-  get 'pages/certification'
+  get 'pages/experience', to: 'pages#experience', as: :experience
+  get 'pages/certification', to: 'pages#certification', as: :certification
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
